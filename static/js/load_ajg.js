db021ae3ff0bf9a2845690044793237a0a86cfa9
K.C <joekcc@gmail.com>
2025-04-28 10:25:59 +0800

    load_ajg

--- static/js/load_ajg.js
+++ static/js/load_ajg.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
     // Load the Excel file
-    fetch('/static/xlsx/cabs_ajg_2024.xlsx')
+    fetch('cabs_ajg_2024.xlsx')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -14,7 +14,8 @@ $(document).ready(function() {
             const worksheet = workbook.Sheets[workbook.SheetNames[0]];
             const json = XLSX.utils.sheet_to_json(worksheet, {header: 1});
 
-            if (json.length === 0) {
+            // Check for empty data
+            if (json.length < 2) {
                 throw new Error('The Excel file is empty or not formatted correctly.');
             }
 
@@ -23,13 +24,11 @@ $(document).ready(function() {
 
             // Populate table with data
             json.forEach((row, index) => {
-                if (index > 0) { // Skip header row
-                    const tr = $('<tr></tr>');
-                    row.forEach(cell => {
-                        tr.append($('<td></td>').text(cell || '')); // Handle undefined cells
-                    });
-                    $('#tableBody').append(tr);
-                }
+                const tr = $('<tr></tr>');
+                row.forEach(cell => {
+                    tr.append($('<td></td>').text(cell || '')); // Handle undefined cells
+                });
+                $('#tableBody').append(tr);
             });
 
             // Initialize DataTable
