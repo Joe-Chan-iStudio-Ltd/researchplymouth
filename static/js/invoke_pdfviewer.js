74d128cea7a8a4d87e350a2065c4bd4814ace59b
K.C <joekcchan@istudio.ltd>
2026-01-01 14:42:18 +0800

    pdfviewer

---
+++ static/js/invoke_pdfviewer.js
@@ -0,0 +1,15 @@
+/* invoke_pdfviewer.js */
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('a.pdfviewer-link');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            const trackingUrl = `/static/html/pdfviewer.html?file=${encodeURIComponent(this.href)}&text=${encodeURIComponent(this.innerText)}`;
+            window.open(trackingUrl, '_blank');
+
+        });
+    });
+});
