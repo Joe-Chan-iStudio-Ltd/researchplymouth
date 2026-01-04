d03f02b80bfce26dcc2deda843427bf25dd5bfa7
K.C <joekcchan@istudio.ltd>
2026-01-04 19:21:44 +0800

    pdfviewer

--- static/js/invoke_pdfviewer.js
+++ static/js/invoke_pdfviewer.js
@@ -2,10 +2,8 @@
 
 $(document).ready(function() {
     $(document).on('click', 'a.pdfviewer-link', function(event) {
-        event.preventDefault(); // Prevent the default link behavior
-        
+        event.preventDefault();
         const trackingUrl = `/static/html/pdfviewer.html?file=${encodeURIComponent(this.href)}&text=${encodeURIComponent(this.innerText)}`;
-        console.log(trackingUrl); // Log the tracking URL
-        window.open(trackingUrl, '_blank'); // Open the new URL in a new tab
+        window.open(trackingUrl, '_blank');
     });
 });
